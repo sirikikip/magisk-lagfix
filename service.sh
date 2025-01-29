3dded1f1c2f2d2ebbc4d0cfa402680a4dee99743
sirikikip <tsaqif.ismailz@gmail.com>
2025-01-29 22:33:06 +0700

    Remove logging

--- service.sh
+++ service.sh
@@ -8,6 +8,3 @@ for i in $(ps -A | grep init | grep subcontext | awk '{print $1}'); do
     echo -1000 > /proc/$i/oom_score_adj
     echo -17 > /proc/$i/oom_adj
 done
-
-# Logging
-echo "Lag fix applied at $(date)" > /data/media/magisklagfix.log
